0ac3ece427a794630b691c7d603cb9ffddb3d8d9
Reika Hirahara <reikahirahara@gmail.com>
2019-12-13 17:08:34 +0900

    initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,88 @@
+const pkg = require('./package')
+const config = require('./.contentful.json')
+
+module.exports = {
+  mode: 'universal',
+
+  /*
+   ** Headers of the page
+   */
+  env: {
+    CTF_SPACE_ID: config.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: config.CTF_CDA_ACCESS_TOKEN,
+    CTF_PERSON_ID: config.CTF_PERSON_ID,
+    CTF_BLOG_POST_TYPE_ID: config.CTF_BLOG_POST_TYPE_ID
+  },
+  head: {
+    title: pkg.name,
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: pkg.description
+      }
+    ],
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }]
+  },
+
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: {
+    color: '#FFFFFF'
+  },
+
+  /*
+   ** Global CSS
+   */
+  css: [],
+
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [],
+
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+  generate: {
+    routes() {
+      return client.getEntries({
+        'content_type': config.CTF_BLOG_POST_TYPE_ID
+      }).then((entries) => {
+        return [...entries.items.map(entry => `posts/${entry.fields.slug}`)]
+      })
+    }
+  },
+}
---
+++ pages/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="section">
+    <div class="columns is-multiline">
+      <div class="column is-half" v-for="(post, index) in posts" :key="index">
+        <div class="card">
+          <header class="card-header">
+            <p class="card-header-title">{{ post.fields.title }}</p>
+          </header>
+          <div class="card-content">
+            <div class="content">
+              {{ post.fields.description }}
+              <br />
+              <small>{{ ( new Date(post.fields.publishDate)).toDateString() }}</small>
+            </div>
+          </div>
+          <footer class="card-footer">
+            <nuxt-link
+              :to="{ name: 'slug', params: { slug: post.fields.slug }}"
+              class="card-footer-item"
+            >Read More</nuxt-link>
+          </footer>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { createClient } from "~/plugins/contentful.js";
+const client = createClient();
+export default {
+  head: {
+    title: "Post"
+  },
+  data() {
+    return {
+      posts: []
+    };
+  },
+  asyncData({ env }) {
+    return client
+      .getEntries({
+        // fetch all blog posts sorted by creation date
+        content_type: env.CTF_BLOG_POST_TYPE_ID,
+        order: "-fields.publishDate"
+      })
+      .then(entries => {
+        return {
+          posts: entries.items
+        };
+      })
+      .catch(console.error);
+  }
+};
+</script>
