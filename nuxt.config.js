41274572a828efe99af2e3b7d9cd4b4dae8192c4
reika0717 <reikahirahara@gmail.com>
2020-01-24 13:41:11 +0900

    modify routing logic

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,22 @@ const client = contentful.createClient({
   accessToken: config.CTF_CDA_ACCESS_TOKEN
 })
 
+const routes = async function() {
+  const [post, tag] = await Promise.all([
+    client.getEntries({
+      content_type: config.CTF_BLOG_POST_TYPE_ID,
+      limit: 1000
+    }),
+    client.getEntries({ content_type: 'tag' })
+  ]);
+  return [
+    ...post.items.map(item => `/entry/${item.sys.id}`),
+    ...tag.items.map(item => `/tags/${item.sys.id}`)
+  ];
+};
+
 module.exports = {
-  mode: 'spa',
+  mode: "spa",
   /*
    ** Headers of the page
    */
@@ -19,31 +33,34 @@ module.exports = {
   },
   head: {
     title: pkg.name,
-    meta: [{
-        charset: 'utf-8'
+    meta: [
+      {
+        charset: "utf-8"
       },
       {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1'
+        name: "viewport",
+        content: "width=device-width, initial-scale=1"
       },
       {
-        hid: 'description',
-        name: 'description',
+        hid: "description",
+        name: "description",
         content: pkg.description
       }
     ],
-    link: [{
-      rel: 'icon',
-      type: 'image/x-icon',
-      href: '/favicon.ico'
-    }]
+    link: [
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: "/favicon.ico"
+      }
+    ]
   },
 
   /*
    ** Customize the progress-bar color
    */
   loading: {
-    color: '#3B8070'
+    color: "#3B8070"
   },
 
   /*
@@ -54,7 +71,7 @@ module.exports = {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ['./plugins/contentful.js'],
+  plugins: ["./plugins/contentful.js"],
 
   /*mi
    ** Nuxt.js modules
@@ -72,24 +89,27 @@ module.exports = {
       // Run ESLint on save
       if (ctx.isDev && ctx.isClient) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/
-        })
+        });
       }
     }
   },
   generate: {
-    routes() {
-      return client.getEntries({
-        'content_type': config.CTF_BLOG_POST_TYPE_ID
-      }).then((entries) => {
-        return [...entries.items.map(entry => `/${entry.sys.id}`)]
-      })
-    }
+    routes
+    // routes() {
+    // return client.getEntries({
+    //   'content_type': config.CTF_BLOG_POST_TYPE_ID
+    // }).then((entries) => {
+    //   return [
+    //     ...entries.items.map(entry => `/${entry.sys.id}`)
+    //   ];
+    // })
+    // }
   },
   router: {
     base: "/nuxt-contentful-test/"
-  },
-}
+  }
+};
